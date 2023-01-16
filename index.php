2297e236b5fc9fa8e1b781359abafbae63e59543
Matthias Plaickner <matthiasplaickner@gmail.com>
2023-01-16 12:49:57 +0100

    css file erstellt und test  button

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <html>
 <head>
+    <link rel="stylesheet" href="style.css">
     <title>Mandor Motors</title>
 </head>
 <body>
+    <form method="post">
+        <input type="submit" value="Test">
+    </form>
+
+
+
     <?php
         echo"hallo";
     ?>
