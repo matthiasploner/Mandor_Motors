f2a3a79894d1e9a11e75c09ce44b9f0a7f04d666
Matthias Plaickner <matthiasplaickner@gmail.com>
2023-01-18 10:23:02 +0100

    tagebuch Seite begonnen

--- tagebuch/index.php
+++ tagebuch/index.php
@@ -8,10 +8,35 @@
         body {
             margin: 0;
             font-family: Arial, Helvetica, sans-serif;
-            background-image: url('car.jpg');
+            background-image: url('../car.jpg');
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: cover;
+        }
+
+        button{
+            align-items: center;
+            background: #FFFFFF;
+            border: 0 solid #E2E8F0;
+            box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+            box-sizing: border-box;
+            color: #1A202C;
+            display: inline-flex;
+            font-family: Inter, sans-serif;
+            font-size: 1rem;
+            font-weight: 700;
+            height: 56px;
+            justify-content: center;
+            line-height: 24px;
+            overflow-wrap: break-word;
+            padding: 24px;
+            text-decoration: none;
+            width: auto;
+            border-radius: 8px;
+            cursor: pointer;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
         }
 
         .topnav {
@@ -124,7 +149,19 @@
         .dropdown-btn {
             display: none;
         }
-
+        .textarea{
+            width: 100%;
+            height: 150px;
+            padding: 12px 20px;
+            box-sizing: border-box;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            background-color: #f8f8f8;
+            font-size: 16px;
+            resize: none;
+            bottom:10px;
+            overflow: scroll;
+        }
         @media only screen and (max-width: 600px) {
             .topnav-right {
                 display: none;
@@ -174,6 +211,8 @@
             .dropdown-btn i {
                 font-size: 24px;
             }
+
+
             .dropdown-content {
                 display: none;
                 position: absolute;
@@ -192,7 +231,7 @@
             }
 
             body {
-                background-image: url('car.jpg');
+
                 background-repeat: no-repeat;
                 background-position: center;
                 background-attachment: fixed;
@@ -200,12 +239,14 @@
                 height:100%;
                 width:100%;
             }
+
         }
     </style>
 </head>
 <body>
 <div class="topnav">
-    <a class="active" href="#"><img src="MM_Icon.png"></a>
+
+    <a class="active" href="#"><img src="../MM_Icon.png"></a>
     <div class="topnav-right">
         <a href="#">About Us</a>
         <a href="login">Account</a>
@@ -230,24 +271,17 @@
         </div>
     </div>
 </div>
-
-<div class="center">
-    <h1>MK1</h1>
-    <p>What colour is your MK1?</p>
+<h1>Unser Tagebuch</h1>
+<div class="textarea">
+    <form method="post">
+            <textarea id="text" name="text" cols="35" rows="4"></textarea>
+            <button onclick=saveText()>What is the time?</button>
+    </form>
 </div>
-
-<div class="navbar">
-    <a href="#">Mandor Motors</a>
-    <a href="#">Privacy & Legal</a>
-    <a href="#">Contact</a>
-    <a href="#">Careers</a>
-    <a href="#">News</a>
-    <a href="#">Locations</a>
-</div>
-
 <script>
-    function dropdownFunction(str) {
-        document.getElementById(str).classList.toggle("show");
+
+    function saveText(){
+
     }
 </script>
 </body>
