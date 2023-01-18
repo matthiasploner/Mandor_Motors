2b8ab9e0aed09abe06258b80f4f7e7fb2bc42dbc
Matthias Plaickner <matthiasplaickner@gmail.com>
2023-01-18 09:15:26 +0100

    css file neu gemacht

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <title>Mandor Motors</title>
+
 </head>
 <body>
     <form method="post">
-        <input type="submit" value="Test">
+        <input type="submit" value="Mandor-Motors">
     </form>
 
 
 
     <?php
-        echo"hallo";
+        echo"Hoi";
     ?>
 </body>
 </html>
