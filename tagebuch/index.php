057c6a5eee9d8e51c151801cc30aa692ceb255e1
Matthias Plaickner <matthiasplaickner@gmail.com>
2023-02-07 09:04:25 +0100

    Javascript Einträge auslesen hinzugefügt

--- tagebuch/index.php
+++ tagebuch/index.php
@@ -24,7 +24,24 @@
     if(isset($_COOKIE['benutzername']) && isset($_COOKIE['token'])){
         verifyCookie();
     echo '
-    <div class="textareaRead col-lg-8 col-lg-offset-2">
+            <div class="top"><h1>Unser Tagebuch</h1></div>
+                <div class="col-lg-2">
+                    <br><br><br><br>
+                    <p>Benutzer und Datum für Eintrag auswählen:</p>
+                        <select id="selectReadUser">
+                            <option>--Alle--</option>
+                            <option>Matthias Plaickner</option>
+                            <option>Simon Ploner</option>
+                            <option>Thomas Reinthaler</option>
+                            <option>Michael Huber</option>
+                            <option>Matthias Ploner</option>
+                        </select>
+                        <input style="resize:none" id="readDate" type="date" name="date">
+                        <div class="col-lg-2">
+                            <button onclick=readText()>Speichern</button>
+                        </div>
+                </div>
+    <div class="textareaRead col-lg-8">
             <div class="top"><h1>Unser Tagebuch</h1></div>
             <textarea class="styleArea form-control" style="resize:none" id="readArea" cols="100" readonly rows="16">
             </textarea>
@@ -36,6 +53,7 @@
     </div>
     <div class="container-fluid">
         <div class="row">
+        
             <div class="textarea">
                 <div class="col-lg-4 col-lg-offset-2">
                     <p>Benutzer und Datum für Eintrag auswählen:</p>
@@ -60,7 +78,8 @@
     </div><br>';}
     else{
         setcookie("destination", "tagebuch", time() + 3600, "/");
-        echo '<div class="top"><h1>Unser Tagebuch</h1></div><div class="col-lg-2">
+        echo '<div class="top"><h1>Unser Tagebuch</h1></div>
+                <div class="col-lg-2">
                     <br><br><br><br>
                     <p>Benutzer und Datum für Eintrag auswählen:</p>
                         <select id="selectUser">
@@ -72,6 +91,9 @@
                             <option>Matthias Ploner</option>
                         </select>
                     <input style="resize:none" id="datum" type="date" name="date">
+                    <div class="col-lg-2">
+                        <button onclick=readText()>Speichern</button>
+                    </div>
                 </div>
                 <div class="textareaRead col-lg-8">         
             <textarea class="styleArea form-control" style="resize:none" id="readArea" cols="100" readonly rows="18">
@@ -88,47 +110,33 @@
 
 
     <script>
+
+
         function readText(){
-            var select = document.getElementById('selectUser');
+            var select = document.getElementById('selectReadUser');
             var selectValue = select.options[select.selectedIndex].value;
             var id=getUser(selectValue);
             var readArea=document.getElementById('readArea');
             readArea.valueOf();
+            var date = document.getElementById("readDate");
             var xhttp = new XMLHttpRequest();
-            xhttp.open("GET", "uploadSQL.php?benutzer="+id+"&message="+message.value+"&date="+date.value,true);   //file.php muss natürlich angepasst werden
+
+            xhttp.open("GET", "readSQL.php?benutzer="+id+"&date="+date.value,true);   //file.php muss natürlich angepasst werden
 
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     values = JSON.parse(xhttp.responseText);
                     console.log(values);
+
+                    document.getElementById('readArea').value = values["Eintragstext"];
                     // values ist hier jetzt ein Objekt bzw. ein Array aus Objekten. Teste dies mit Ausgabe: console.log(values);
                 }
             };
             xhttp.send();
-            document.getElementById('message').value = '';
+
         }
 
-        function getUser(name){
-            var id;
-            if(name=="Michael Huber"){
-                id=5;
-            }
-            if(name=="Matthias Plaickner"){
-                id=2;
-            }
-            if(name=="Matthias Ploner"){
-                id=1;
-            }
-            if(name=="Simon Ploner"){
-                id=4;
-            }
-            if(name=="Thomas Reinthaler"){
-                id=3;
-            }if(name=="--Alle--"){
-                id=6;
-            }
-            return id;
-        }
+
 
         function saveText(){
             var select = document.getElementById('selectUser');
@@ -155,6 +163,27 @@
             xhttp.send();
             document.getElementById('message').value = '';
 
+        }
+        function getUser(name){
+            var id;
+            if(name=="Michael Huber"){
+                id=5;
+            }
+            if(name=="Matthias Plaickner"){
+                id=2;
+            }
+            if(name=="Matthias Ploner"){
+                id=1;
+            }
+            if(name=="Simon Ploner"){
+                id=4;
+            }
+            if(name=="Thomas Reinthaler"){
+                id=3;
+            }if(name=="--Alle--"){
+                id=6;
+            }
+            return id;
         }
     </script>
 
