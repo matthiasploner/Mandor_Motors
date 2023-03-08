5bbed73c27b301735fa7190d40493333d66c9389
Michael Huber <Michael.Huber_3006@snets.it>
2023-03-08 09:32:30 +0100

    Mobile size fixed

--- tagebuch/functions.js
+++ tagebuch/functions.js
@@ -182,3 +182,13 @@ function getUser(name){
     }
     return id;
 }
+
+function dropdownFunction() {
+    document.getElementById("pcDropdown").classList.toggle("show");
+}
+function navopen() {
+    document.getElementById("mobile-nav").style.width = "100%";
+}
+function navclose() {
+    document.getElementById("mobile-nav").style.width = "0%";
+}
