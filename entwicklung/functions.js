163378d98d9fe029a3714b1303b458bbb2532c2e
Michael Huber <Michael.Huber_3006@snets.it>
2023-03-13 11:49:34 +0100

    Enwicklung Page Slider Added + Center Buttons of Main Page Linked

---
+++ entwicklung/functions.js
@@ -0,0 +1,30 @@
+function dropdownFunction() {
+    document.getElementById("pcDropdown").classList.toggle("show");
+}
+function navopen() {
+    document.getElementById("mobile-nav").style.width = "100%";
+}
+function navclose() {
+    document.getElementById("mobile-nav").style.width = "0%";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function arrowNextSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide-1");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
