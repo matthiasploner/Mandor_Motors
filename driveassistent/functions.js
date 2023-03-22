3a589eee83301823946a51e5c8d3c886f90de190
Michael Huber <Michael.Huber_3006@snets.it>
2023-03-22 08:33:13 +0100

    drive-assistent mobile fixed

--- driveassistent/functions.js
+++ driveassistent/functions.js
@@ -50,3 +50,13 @@ function lichtState(){
         .catch(error => console.error(error));
     setTimeout(lichtState, 500);
 }
+
+function dropdownFunction() {
+    document.getElementById("pcDropdown").classList.toggle("show");
+}
+function navopen() {
+    document.getElementById("mobile-nav").style.width = "100%";
+}
+function navclose() {
+    document.getElementById("mobile-nav").style.width = "0%";
+}
